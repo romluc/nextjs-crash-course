4a33699c3f2c221cc93c3e85e1ede594fe47df89
romluc <romulofilhofilho@gmail.com>
2020-04-29 23:24:39 -0300

    Create Next boilerplate

---
+++ .next/static/development/pages/about.js
@@ -0,0 +1,93 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/about.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=%2FUsers%2Fromulo%2FDocuments%2Fdev%2FTraversy%2Fnextjs-crash-course%2Fpages%2Fabout.js&hotRouterUpdates=true!./":
+/*!*******************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=%2FUsers%2Fromulo%2FDocuments%2Fdev%2FTraversy%2Fnextjs-crash-course%2Fpages%2Fabout.js&hotRouterUpdates=true ***!
+  \*******************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/about",
+      function () {
+        var mod = __webpack_require__(/*! ./pages/about.js */ "./pages/about.js");
+        if (true) {
+          module.hot.accept(/*! ./pages/about.js */ "./pages/about.js", function () {
+            if (!next.router.components["/about"]) return;
+            var updatedPage = __webpack_require__(/*! ./pages/about.js */ "./pages/about.js");
+            next.router.update("/about", updatedPage);
+          });
+        }
+        return mod;
+      }
+    ]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_2adc2403d89adc16ead0 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_2adc2403d89adc16ead0 */ "dll-reference dll_2adc2403d89adc16ead0"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _this = undefined,
+    _jsxFileName = "/Users/romulo/Documents/dev/Traversy/nextjs-crash-course/pages/about.js";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return __jsx("div", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 1,
+      columnNumber: 22
+    }
+  }, "About");
+});
+
+/***/ }),
+
+/***/ 2:
+/*!***********************************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fabout&absolutePagePath=%2FUsers%2Fromulo%2FDocuments%2Fdev%2FTraversy%2Fnextjs-crash-course%2Fpages%2Fabout.js&hotRouterUpdates=true ***!
+  \***********************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fabout&absolutePagePath=%2FUsers%2Fromulo%2FDocuments%2Fdev%2FTraversy%2Fnextjs-crash-course%2Fpages%2Fabout.js&hotRouterUpdates=true! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=%2FUsers%2Fromulo%2FDocuments%2Fdev%2FTraversy%2Fnextjs-crash-course%2Fpages%2Fabout.js&hotRouterUpdates=true!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_2adc2403d89adc16ead0":
+/*!*******************************************!*\
+  !*** external "dll_2adc2403d89adc16ead0" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_2adc2403d89adc16ead0;
+
+/***/ })
+
+},[[2,"static/runtime/webpack.js"]]]);
+//# sourceMappingURL=about.js.map
